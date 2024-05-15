18ed73b21da08f8e0bc7fd235f42b2ea70690d8d
Philip Henning <philiphenning@outlook.com>
2024-05-15 23:27:52 +0100

    added 'For loops' vs 'while loops'

---
+++ forLoopVsWhileLoop/app.js
@@ -0,0 +1,37 @@
+let whileListen = document.getElementById("whileButton");
+whileListen.addEventListener("click", whileLoop);
+
+function whileLoop()
+{  
+    let whileChoice = document.getElementById("whileChoice").value;
+    
+    if(whileChoice == 0 || whileChoice == null){
+
+        document.getElementById("whileResult").innerHTML = "do not leave blank or set as 0";
+    
+    } else{
+        
+        let counter = 1;
+        let string = "";
+        
+        while(counter <= whileChoice)
+            {
+                string = string + counter;
+                console.log(`${string} not equal to ${whileChoice}`);
+                string = string + ", ";
+                counter++;
+            }
+
+        whileWrite(string + "the counter has reached your value!")
+        console.log(string + "the counter has reached your value!")
+    }
+
+}
+
+function whileWrite(counter)
+{
+    let whileResult = document.getElementById("whileResult");
+    whileResult.innerHTML = counter;
+}
+
+
